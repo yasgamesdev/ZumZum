4d38f4963e2db9d70d1250bfde7a4687520cf947
Yas <yasgamesdev@gmail.com>
2018-04-24 23:28:04 +0900

    Add ZumGenerator

---
+++ Assets/Scripts/Zum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zum : MonoBehaviour
+{
+    public ZumType type { get; private set; }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Init(ZumType type, Sprite sprite)
+    {
+        this.type = type;
+        GetComponentInChildren<SpriteRenderer>().sprite = sprite;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+    }
+}
---
+++ Assets/Scripts/ZumGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZumGenerator : MonoBehaviour
+{
+    public GameObject zumPrefab;
+    public Sprite[] zumSprites;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int num = Random.Range(0, zumSprites.Length);
+
+            GameObject zum = Instantiate(zumPrefab, transform);
+            zum.GetComponent<Zum>().Init((ZumType)num, zumSprites[num]);
+        }
+    }
+}
